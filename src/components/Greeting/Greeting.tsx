7a8348c59aea3f88a62068384a681e3247bb02d6
Stephen Barkan <stephen@doist.com>
2021-09-10 11:02:19 -0500

    bleh

--- src/components/Greeting/Greeting.tsx
+++ src/components/Greeting/Greeting.tsx
@@ -2,8 +2,8 @@ import { useEffect } from 'react';
 import useFetch from 'hooks/useFetch';
 const key = '4f6eb44e289844e4b846c2b312cc2c1d';
 const mapId = '40570';
-const stpIdNorth = '30111';
-const stpIdSouth = '30112';
+// const stpIdNorth = '30111';
+// const stpIdSouth = '30112';
 
 const urlAppend = `?key=${key}&mapid=${mapId}&outputType=JSON`;
 
