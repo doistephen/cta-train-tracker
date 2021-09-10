838eb6848fce16e11fd9f213bea19b9ae1a6bdd5
Stephen Barkan <stephen@doist.com>
2021-09-10 10:55:47 -0500

    test

--- src/components/Greeting/Greeting.tsx
+++ src/components/Greeting/Greeting.tsx
@@ -1,6 +1,26 @@
+import { useEffect } from 'react';
+import useFetch from 'hooks/useFetch';
+const key = '4f6eb44e289844e4b846c2b312cc2c1d';
+const mapId = '40570';
+const stpIdNorth = '30111';
+const stpIdSouth = '30112';
+
+const urlAppend = `?key=${key}&mapid=${mapId}&outputType=JSON`;
+
 export const Greeting = () => {
+  const { get, loading } = useFetch(
+    'http://lapi.transitchicago.com/api/1.0/ttarrivals.aspx',
+  );
+
+  useEffect(() => {
+    get(urlAppend)
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((error) => console.log(error));
+  }, []);
   return (
-    <div className="bg-gray-700 text-gray-100 text-center py-2 text-xl font-bold">
+    <div className="py-2 text-xl font-bold text-center text-gray-100 bg-gray-700">
       Say hi
     </div>
   );
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,7 +5,7 @@ function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <title>Title - MAKE ME DYNAMIC PLEASE</title>
+        <title>California Blue Line Arrival Tracker</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />;
       </Head>
