d246010ee3366bfbfd94ca0aa5e39e76eae70f7f
Stephen Barkan <stephen@doist.com>
2021-09-12 16:40:58 -0500

    adksfja

--- next.config.js
+++ next.config.js
@@ -5,6 +5,13 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 });
 
 module.exports = withPlugins([[withBundleAnalyzer]], {
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
   experimental: {
     esmExternals: true,
   },
