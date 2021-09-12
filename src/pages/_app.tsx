9abd93fff9ab13954919bb637c075bc0d6edccfc
Stephen Barkan <stephen@doist.com>
2021-09-12 16:25:57 -0500

    adds app icon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,8 +6,16 @@ function MyApp({ Component, pageProps }) {
     <>
       <Head>
         <title>California Blue Line Arrival Tracker</title>
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta
+          name="viewport"
+          content="initial-scale=1.0, width=device-width, viewport-fit=cover, user-scalable=no"
+        />
         <link rel="shortcut icon" href="/favicon.svg" />
+        <link rel="manifest" href="/site.webmanifest" />
+        <meta
+          name="apple-mobile-web-app-status-bar-style"
+          content="black-translucent"
+        />
       </Head>
       <Component {...pageProps} />
     </>
